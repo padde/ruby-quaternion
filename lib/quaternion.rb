a3181f11b7d09ad92cca272b0cf3886912d2639c
Patrick Oscity <patrick.oscity@gmail.com>
2011-06-08 16:29:19 +0200

    external constructor, round function, cleaning up

--- lib/quaternion.rb
+++ lib/quaternion.rb
@@ -1,5 +1,15 @@
 require 'mathn'
 
+def Quaternion *args
+  if args[0].is_a? Quaternion
+    args[0]
+  elsif args[0].is_a? Numeric and args[1].is_a? Vector
+    Quaternion.new args[0], *args[1]
+  else
+    Quaternion.new *args
+  end
+end
+
 class Quaternion < Numeric
   attr_accessor :a, :b, :c, :d
     
@@ -15,26 +25,28 @@ class Quaternion < Numeric
   end
   alias :re :real
     
-  def imaginary
+  def imag
     Vector[@b,@c,@d]
   end
-  alias :im :imaginary
+  alias :im :imag
+  alias :imaginary :imag
   
   def length
     Math::sqrt( @a**2 + @b**2 + @c**2 + @d**2 )
   end
   alias :norm :length
   
-  def conjugate
+  def conj
     Quaternion.new( @a, -@b, -@c, -@d )
   end
+  alias :conjugate :conj
   
   def inverse
-    self.conjugate / self.length**2
+    conj / length**2
   end
   
   def unify
-    self / self.length
+    self / length
   end
   
   def + other
@@ -47,10 +59,12 @@ class Quaternion < Numeric
   
   def * other
     if other.is_a? Quaternion
-      Quaternion.new( @a*other.a - @b*other.b - @c*other.c - @d*other.d,
-                      @a*other.b + @b*other.a + @c*other.d - @d*other.c,
-                      @a*other.c - @b*other.d + @c*other.a + @d*other.b,
-                      @a*other.d + @b*other.c - @c*other.b + @d*other.a )
+      Quaternion.new(
+        @a*other.a - @b*other.b - @c*other.c - @d*other.d,
+        @a*other.b + @b*other.a + @c*other.d - @d*other.c,
+        @a*other.c - @b*other.d + @c*other.a + @d*other.b,
+        @a*other.d + @b*other.c - @c*other.b + @d*other.a
+      )
     else 
       Quaternion.new( @a*other, @b*other, @c*other, @d*other )
     end
@@ -72,26 +86,33 @@ class Quaternion < Numeric
     end
   end
   
+  def round ndigits=0
+    Quaternion.new(
+      @a.round(ndigits),
+      @b.round(ndigits),
+      @c.round(ndigits),
+      @d.round(ndigits)
+    )
+  end
+  
   def to_s
     "#{@a}#{pip @b}i#{pip @c}j#{pip @d}k"
   end
   
   def self.rotate point, axis, angle
     phi_half = (angle/180.0*Math::PI)/2.0
-    
     p = Quaternion.new( 0.0, *point )
-  
+    
     axis = axis.collect{ |c| Math::sin(phi_half)*c }
     r = Quaternion.new( Math::cos(phi_half), *axis )
     
     p_rotated = r * p * r.inverse
-    
-    Vector[p_rotated.b, p_rotated.c, p_rotated.d]
+    p_rotated.imag
   end
   
 private
   def plus_if_positive x
-    if x > 0
+    if x >= 0
       "+#{x}"
     else
       x.to_s
